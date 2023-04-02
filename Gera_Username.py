0f16f454df25772600eee7ea39150009f687008e
Vitoria Mercado <andrader.vitoria@gmail.com>
2023-04-02 20:48:53 -0300

    Adicionado Gera_Username.py porem nao finalizado

---
+++ Gera_Username.py
@@ -0,0 +1,40 @@
+"""
+Python: Usernames
+Faça um programa capaz de gerar usernames e senhas para alunos da FATEC de Ribeirão Preto.
+O programa recebe como entrada o nome completo do aluno e produzir um username contendo:
+• A primeira letra do nome e o sobrenome
+O resultado deve ser armazenado em um estrutura da sua preferência: Tupla, Lista, Dicionário ou Conjunto.
+O programa deve garantir que não sejam gerados username duplicados
+As senhas provisórias deve conter no mínimo 8 caracteres (números, letras e símbolos) com máxima segurança.
+"""
+import os
+
+def fLimpaTela():
+    os.system('cls')
+
+def fGeraUsername(pNomeCompleto):
+    # Transforma a string recebida no username padrão primeironome.ultimosobrenome
+    vPrimeiraLetraNome = pNomeCompleto[0]
+    vNomeSeparado = pNomeCompleto.split()
+    vUltimoSobrenome = vNomeSeparado[-1]
+    vUsernameTemp = vPrimeiraLetraNome + "." + vUltimoSobrenome
+    # Aqui começa a validação da lista de alunos
+    vListaUsernames = []
+    vContador = 1
+    for i in vListaUsernames:
+        if vUsernameTemp == i:
+            vUsernameTemp = vUsernameTemp + str(vContador)
+            vContador += 1
+    vUsernameFinal = vUsernameTemp + "@fatec.sp.gov.br"
+    vListaUsernames.append(vUsernameFinal)
+    return vUsernameFinal
+
+def main():
+    fLimpaTela()
+    vQtdeAlunosRegistrados = int(input("Informe quantos alunos serão registrados: "))
+    for i in range(vQtdeAlunosRegistrados):
+        vNomeCompleto = input("\nInforme o nome completo: ")
+        vSenha = input("Informe a senha, a mesma deve atender aos seguintes requisitos:\n- mínimo 8 caracteres\n- Números\n- Letras\n- Símbolos\n: ")
+        print(f"Seu username é: {fGeraUsername(vNomeCompleto)}")
+
+main()
